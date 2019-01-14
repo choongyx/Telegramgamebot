b5ddf29b6fcfeb09d51e7d15ccd2b6f40bb091b9
KerrynEer <KerrynEer@users.noreply.github.com>
2019-01-14 22:23:34 +0800

    Change name

--- main.js
+++ main.js
@@ -25,5 +25,5 @@ bot.on('message', (msg) => {
   const chatId = msg.chat.id;
 
   // send a message to the chat acknowledging receipt of their message
-  bot.sendMessage(chatId, 'YO U WANNA PLAY A GAME?');
+  bot.sendMessage(chatId, 'YO U WANNA PLAY SLAPTHEENEMY?');
 });
