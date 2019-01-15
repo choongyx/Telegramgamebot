e8c286fccd596538beeb1042eb0295cd77052a84
KerrynEer <KerrynEer@users.noreply.github.com>
2019-01-15 23:24:56 +0800

    Add play and share button

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ const token = '729085676:AAEQFP1BRcCSlq7kvHeEreyzzbpGzuS_blM';
 // Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(token, {polling: true});
 
-const gameName = "SLAPTHEENEMY"
+const gameName = "SlaptheEnermy";
 
 // Matches "/echo [whatever]"
 bot.onText(/\/echo (.+)/, (msg, match) => {
@@ -28,18 +28,105 @@ bot.onText(/\/echo (.+)/, (msg, match) => {
 //   const chatId = msg.chat.id;
 
 //   // send a message to the chat acknowledging receipt of their message
-//   bot.sendMessage(chatId, 'YO U WANNA PLAY SLAPTHEENEMY?');
+//   bot.sendMessage(chatId, 'YO U WANNA PLAY SlaptheEnermy?');
 // });
 
 //HELP message
-bot.onText(/help/, (msg) => bot.sendMessage(msg.from.id, "This bot implements a SLAPTHEENEMY game. Say /start (or /SLAPTHEENEMY) if you want to play."));
+bot.onText(/help/, (msg) => bot.sendMessage(msg.from.id, "This bot implements a SlaptheEnermy game. Say /start or /SlaptheEnermy if you want to play."));
 
-//START or SLAPTHEENEMY message
+//START or SlaptheEnermy message
 //need changes
-bot.onText(/start|SLAPTHEENEMY/, (msg) => {
+bot.onText(/start|SlaptheEnermy/, (msg) => {
   const chatId = msg.chat.id;
 
   // send a message to the chat acknowledging receipt of their message
-  bot.sendMessage(chatId, 'YO U WANNA PLAY SLAPTHEENEMY?');
+  bot.sendMessage(chatId, 'YO U WANNA PLAY SlaptheEnermy? Type /play SlaptheEnermy');
 });
 
+bot.onText( /\/play (.+)/, function( msg, match ) {
+
+  var fromId = msg.from.id;
+
+  switch( match[1] ) {
+
+      case "SlaptheEnermy":
+
+          bot.sendGame(
+
+              fromId,
+
+              "SlaptheEnermy",
+
+              {
+
+                  reply_markup: JSON.stringify({
+
+                      inline_keyboard: [
+
+                          [ { text: "Play", callback_game: JSON.stringify( { game_short_name: "SlaptheEnermy" } ) } ],
+                        
+                          [ { text: "Share", url: "http://t.me/Slapurface_bot?game=SlaptheEnermy" } ]
+
+                      ]
+
+                  })
+
+              }
+
+          );
+
+          break;
+
+      default:
+
+          bot.sendMessage( fromId, "Sorry " + msg.from.first_name + ", but this game doesn’t exist.." );
+
+  }
+
+} );
+
+// button = new InlineKeyboardButton().setText("Play solo.")
+//     .setCallbackGame(
+//         new CallbackGame( 
+//             new JSONObject("{game_short_name: \"" + game.getShortName() + "\"}"))
+//         );
+
+bot.on("callback_query", function (query) {
+  
+  if (query.game_short_name != gameName) {
+    bot.answerCallbackQuery(query.id, "Sorry, '" + query.game_short_name + "' is not available.");
+  } else {
+    queries[query.id] = query;
+    let gameurl = "http://t.me/Slapurface_bot?game=SlaptheEnermy  id="+query.id;
+    bot.answerCallbackQuery({
+      callback_query_id: query.id,
+      url: gameurl
+    });
+  }
+});
+
+// bot.on( "callback_query", function( cq ) {
+
+//   if ( cq.game_short_name ) {
+
+//       switch( cq.game_short_name ) {
+
+//           case "SlaptheEnermy":
+
+//               bot.answerCallbackQuery( cq.id, undefined, false, { url: "URL_DE_NUESTO_JUEGO" } );
+
+//               return;
+
+//       }
+
+//       bot.answerCallbackQuery( cq.id, "Sorry, '" + cq.game_short_name + "' is not available.", true );
+
+//   }
+
+// } );
+
+bot.on( "inline_query", function( iq ) {
+
+  bot.answerInlineQuery( iq.id, [ { type: "game", id: "0", game_short_name: "SlaptheEnermy" } ] );
+
+} );
